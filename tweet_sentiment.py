ab73257451f1cef306f6e4a680bb1570b24b15fd
Richard Leon Terrell <rickalo801@gmail.com>
2014-07-09 23:31:22 -0400

    Hacking away at twitter data. Not usable yet. Just saving to work on from other machines.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,4 +1,6 @@
 import sys
+import json
+from pprint import pprint
 
 def hw():
     print 'Hello, world!'
@@ -7,11 +9,40 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    #sent_file = open("AFFIN-111.txt")
+    read_affin()
+    tweet_file = open(sys.argv[1])
+    #print sys.argv[1]
+    #print tweet_file
+    tweet_data = tweet_file.readlines()
+    tweets = read_tweets(tweet_data)
+    print len(tweets)
+    for tweet in tweets[10:16]:
+        print tweet["text"].encode('utf-8')
+    print tweets[10].keys()
+    #print tweet_data
+    #tweet = json.loads(tweet_data)
+    #pprint(tweets[10])
+    #print tweet_file.read()
+
+def read_tweets(tweet_data):
+    tweets = []
+    #print tweet_data
+    #print "num tweets " + str(len(tweet_data.split("\n")))
+    print "num " + str(len(tweet_data))
+    for line in tweet_data:
+        #print line
+        tweets.append(json.loads(line))
+    return tweets
+
+def read_affin():
+    afinnfile = open("AFINN-111.txt")
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    #print scores.items() # Print every (term, score) pair in the dictionary
 
 if __name__ == '__main__':
     main()
